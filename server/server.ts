f43164f8cfd6f01cbbd8b1ba39d3ab7010e68d8b
Price Comstock <price.comstock@gmail.com>
2020-10-10 15:59:35 -0400

    little more express

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello world");
+import express, { Request, Response, NextFunction } from "express";
+import bodyParser from "body-parser";
+const { json } = bodyParser;
+
+const app = express();
+
+app.use(json());
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(3000);
