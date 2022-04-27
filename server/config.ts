03d535d593ceeea2ce0c8b531dafddddc6920c87
Price <price.comstock@gmail.com>
2022-04-27 09:16:04 -0400

    remove console log

--- server/config.ts
+++ server/config.ts
@@ -3,11 +3,6 @@ export const MTA_API_KEY: string = process.env.MTA_API_KEY ?? "";
 export const UPDATE_INTERVAL_MS: number = process.env.UPDATE_INTERVAL_MS
   ? Number(process.env.UPDATE_INTERVAL_MS)
   : 30000;
-console.log(
-  "🔴====> ~ file: config.ts ~ line 4 ~ UPDATE_INTERVAL_MS",
-  process.env.UPDATE_INTERVAL_MS,
-  UPDATE_INTERVAL_MS
-);
 
 if (UPDATE_INTERVAL_MS < 10000) {
   throw new Error(
