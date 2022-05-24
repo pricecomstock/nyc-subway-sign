3e8e07f02603a85b1451fefbb698d2b3b1bb5670
Price <price.comstock@gmail.com>
2022-05-23 23:34:26 -0400

    arrange stations nicely

--- src/components/Stations.svelte
+++ src/components/Stations.svelte
@@ -1,64 +1,157 @@
 <script lang="typescript">
-  import { getTrainColorFGClass } from "../helpers/color"
-	import TrainIcon from "./TrainIcon.svelte"
-  
-  import {createEventDispatcher} from "svelte";
+  const boroughNames = {
+    bk: "Brooklyn",
+    m: "Manhattan",
+    bx: "Bronx",
+    q: "Queens",
+    si: "Staten Island",
+  };
+
+  // For ordering boroughs vaguely geographically
+  const boroughOrder = {
+    bx: 0,
+    m: 1,
+    bk: 2,
+    q: 3,
+    si: 3,
+  };
+
+  import { getTrainColor } from "../helpers/color";
+  import TrainIcon from "./TrainIcon.svelte";
+
+  import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
 
   export let train = "";
-  export let stations = []
+  export let stations = [];
 
   let selectedIndex = -1;
 
   $: selectedStation = stations[selectedIndex];
-  $: trainColorClass = getTrainColorFGClass(train)
+  $: {
+    document.documentElement.style.setProperty(
+      "--selected-train-color",
+      `var(${getTrainColor(train)})`
+    );
+  }
+  $: stationsByBorough = [
+    ...stations
+      .reduce((boroughMap, station) => {
+        const normalizedBorough = station.borough.toLowerCase();
+        const stationsForBorough = boroughMap.get(normalizedBorough) ?? [];
+        stationsForBorough.push(station);
+        boroughMap.set(normalizedBorough, stationsForBorough);
+        return boroughMap;
+      }, new Map())
+      .entries(),
+  ].sort((a, b) => {
+    return boroughOrder[a[0]] - boroughOrder[b[0]];
+  });
 
-  function selectIndex(index: number) {
-    selectedIndex = index;
+  function selectStation(station) {
     dispatch("select", {
-      station: stations[selectedIndex]
-    })
+      station,
+    });
   }
 </script>
 
+<div class="station-picker">
+  {#if stations}
+    <div class="selected-train-title">
+      <TrainIcon {train} size="1em" /> Stations
+    </div>
+    <div class="boroughs-list-container">
+      {#each stationsByBorough as [borough, stationGroup]}
+        <div class="stations-list">
+          <h2 class="borough-name">{boroughNames[borough]}</h2>
+          {#each stationGroup as station, i}
+            <div on:click={() => selectStation(station)} class="station-button">
+              <div class="station-select-icon-group">
+                {#each station.trains as stationTrain}
+                  <TrainIcon train={stationTrain} size="1.2rem" />
+                {/each}
+              </div>
+              <div class="station-name">
+                <p>{station.stopName}</p>
+              </div>
+            </div>
+          {/each}
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div>
+
 <style>
-  .icon {
+  .train-select-icon {
     margin-right: 0.5rem;
   }
 
-  h1 {
-    display: flex;
-    align-items: center;
+  .selected-train-title {
+    text-align: center;
+    margin: auto;
+    font-size: 2rem;
+    font-weight: bold;
   }
 
-  div {
-    max-width: 30ch;
+  div.station-picker {
     margin: auto;
   }
-	.stations-list {
-    text-align: left;
+  .boroughs-list-container {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
   }
-  li {
+
+  .stations-list {
+    text-align: left;
+    width: min(50ch, 100%);
+    margin: 0 0.5rem;
+  }
+
+  .station-button:first-of-type {
+    border-top: 1px solid #aaa;
+  }
+
+  .station-button {
+    padding: 0.1rem 0;
+    border-bottom: 1px solid #aaa;
+    font-size: 1.2rem;
+
+    display: flex;
+
     cursor: pointer;
     transition: color 0.2s;
+    transition: border-width 0.2s;
+    transition: margin 0.2s;
   }
-  li:not(:hover) {
-    color: black !important;
+  .station-select-icon-group {
+    display: inline-block;
+    width: max(8ch, 15%);
+    flex-basis: 8ch;
+    flex-shrink: 1;
+    margin-right: 0.5rem;
+    text-align: right;
+  }
+
+  .station-name {
+    display: inline-block;
+    overflow-wrap: break-word;
+  }
+
+  .station-name > p {
+    margin: 0;
+    padding: 0;
+  }
+
+  .station-button:nth-child(odd) {
+  }
+
+  .station-button:hover {
+    background: #fafafa;
+    /* color: var(--selected-train-color); */
+    border-right: 5px solid var(--selected-train-color);
+    font-weight: bold;
+    text-decoration-style: wavy;
   }
 </style>
-
-  
-<div>
-  {#if stations}
-  <h1>
-    <span class="icon"><TrainIcon {train} size="50px"></TrainIcon></span> Stations
-  </h1>
-  <ul class="stations-list">
-    {#each stations as station, i}
-    <li on:click={() => selectIndex(i)} class={`${trainColorClass}`}>
-      {station.stopName}
-    </li>
-    {/each}
-  </ul>
-  {/if}
-</div>
